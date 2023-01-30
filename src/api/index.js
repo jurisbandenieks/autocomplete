1fea26b2bb8daa3b07396ee30690d3c7f60150fd
Juris Bandenieks <jurisbandenieks90@gmail.com>
2023-01-30 07:26:39 -0800

    map values to standartize

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+export async function fetchLocation() {
+  return await axios.get("../assets/initLocation.json");
+}
+
 export async function fetchLocations(inputText) {
   const queryParams = new URLSearchParams({
     key: "pk.7eb1ffef27f722e89d8ee135078399b4",
@@ -8,5 +12,11 @@ export async function fetchLocations(inputText) {
   });
   const url = "https://api.locationiq.com/v1/autocomplete.php?";
 
-  return await axios.get(url + queryParams);
+  const { data } = await axios.get(url + queryParams);
+
+  const dataWithId = data.map((el) => {
+    return { ...el, id: el.place_id, label: el.display_name };
+  });
+
+  return dataWithId;
 }
